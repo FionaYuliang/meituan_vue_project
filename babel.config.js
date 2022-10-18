d97add6b0b581a8dd7ef7c1d74059974801a3121
FionaYuliang <yangmingming94@gmail.com>
2022-10-18 15:04:27 +0800

    初始化配置

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,18 @@
 module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
-  ]
+  ],
+  // babel.config.js
+  plugins: [ [
+    'import', {
+          libraryName: 'vant',
+          libraryDirectory: 'es',
+          style: true,
+    },
+    'vant', 
+    ],
+  ],
 }
+
 
 
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,16 @@
 const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+// module.exports = defineConfig({
+//   transpileDependencies: true,
+// })
 
+module.exports = ({
+  lintOnSave: false,
+  css: {
+    loaderOptions: {
+      postcss: {
+        plugins: [
+          require('postcss-pxtorem')({ rootValue: 16 , propList: ['*']}),
+], 
+},
+}, },
+})
