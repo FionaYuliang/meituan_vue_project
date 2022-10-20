4a47ab4685b61ed5d15917b206db1ac186526c5e
FionaYuliang <yangmingming94@gmail.com>
2022-10-20 20:01:26 +0800

    首页第二部分建立子组件和孙子组件,并且完成静态页面和组件传值

---
+++ src/pages/myHome/components/Store.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="store_list">
+        <div v-for="(i, index) in store_list" :key="index" class="store_list_item">
+            <StoreItem :item="i"></StoreItem>
+        </div>
+    </div>
+</template>
+<script>
+import StoreItem from "./StoreItem.vue";
+export default {
+    props:["store_list"],
+    components:{
+    StoreItem,
+},
+};
+</script>
+
+<style lang="less" scoped>
+.store_lsit_item {
+    margin-top: 10px;
+}
+</style>
