cee98d7e26e9f8c0b26b102ae230c421f33b2663
FionaYuliang <yangmingming94@gmail.com>
2022-10-24 14:02:30 +0800

    搭建账号管理页面结构

--- src/pages/myCenter/Userinfo.vue
+++ src/pages/myCenter/Userinfo.vue
@@ -1,5 +1,14 @@
 <template>
-    账号管理页面
+    <div class="user-edit">
+        <header title="账号管理"></header>
+        <div class="input-item">
+            <van-field v-model="username" label="昵称" placeholder="请输入用户名"/>
+            <van-field v-model="intro" label="个性签名" placeholder="请输入个性签名"/>
+            <van-field v-model="password" type="password" label="修改密码" placeholder="请输入密码"/>
+        </div>
+        <van-button type="primary" round color="#ffc400" block class="save-btn" @click="save">保存</van-button>
+            <van-button type="primary" round color="#ffc400" block class="save-btn" @click="logout">退出登录</van-button>
+    </div>
 </template>
 <script>
 import { onMounted, reactive, toRefs }  from 'vue';
@@ -29,4 +38,12 @@ setup(){
 }   
 </script>
 <style lang='less' scoped>
+/deep/ .van-button--danger {
+  background-color: #ffc400;
+  border-color: #ffc400;
+}
+.save-btn {
+  width: 80%;
+  margin: 20px auto;
+}
 </style>
